a20c6d76dfdc678c2be0be0216d1ec8ffacf35bf
zwashburn2003 <zwashburn03@gmail.com>
2024-12-05 06:58:18 -0800

    Menu Bar and 2 Column

--- first.py
+++ first.py
@@ -1,15 +1,18 @@
 import sqlite3
 import tkinter as tk
-from tkinter import messagebox
 # Import SQL for Database read and tkinter for GUI
 
 root = tk.Tk()
 root.title("Simple GUI Example")
 
 menubar = tk.Menu(root, background='black',foreground='black',activebackground='white', activeforeground='black')
-file = tk.Menu(menubar, tearoff=1,background='white', foreground='black')
+file = tk.Menu(menubar, tearoff=0,background='white', foreground='black')
 file.add_command(label="New")
 menubar.add_cascade(label="File", menu=file)
+
+database = tk.Menu(menubar, tearoff=0,background='blue', foreground='black')
+database.add_command(label="Select")
+menubar.add_cascade(label="Database", menu=database)
 
 frame = tk.Frame(root)
 frame.pack(side="left",pady=20)
@@ -21,6 +24,17 @@ scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=listbox.yview)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 listbox.config(yscrollcommand=scrollbar.set)
 
+frame = tk.Frame(root)
+frame.pack(side="left",pady=20)
+
+roads = tk.Listbox(frame, width=50, height=100)
+roads.pack(side=tk.LEFT, fill=tk.BOTH)
+
+scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=roads.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+roads.config(yscrollcommand=scrollbar.set)
+
+
 label = tk.Label(root, text="Hello, Tkinter!")
 label.pack(side="left", pady=10)
 
@@ -29,12 +43,13 @@ conn = sqlite3.connect(dbfile)
 cursor= conn.cursor()
 
 
-cursor.execute("SELECT * FROM artists")
+cursor.execute("SELECT * FROM albums")
 
 rows = cursor.fetchall()
 
 for row in rows:
     listbox.insert(tk.END, row)
+
 root.config(menu=menubar)
 root.mainloop()
 conn.close()
